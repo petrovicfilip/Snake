ad5a3970ca8bd2638fc359ad13f707dc17fcf4d8
Filip Petrovic <filip.petrovic@elfak.rs>
2025-04-04 16:15:21 +0200

    Dodato bodovanje, bodovanje za sada ide u konzolu jer imam problema sa TTF bibliotekom, dodata jednostavna xor cyper enkripcija bodova...

--- main.c
+++ main.c
@@ -2,17 +2,20 @@
 #include <SDL3/SDL.h>
 #include <stdlib.h>
 #include <time.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_ttf/SDL_textengine.h>
 
 #define BLOCK_SIZE 20
 #define ROWS 21
 #define COLS 31
+#define BASE_TIME_BONUS 15
+
+int time_bonus = BASE_TIME_BONUS;
+
+char highscore[] = "T4iZ69S-H1gh42oR38 ";
+char key[] = "%%&#";
 
 bool occupied_grid_blocks[ROWS * COLS];
-       
-typedef struct
-{
-    Uint32 value;
-} Fruit;
 
 typedef struct SnakePart SnakePart;
 struct SnakePart
@@ -31,6 +34,83 @@ typedef struct
     SnakePart* snake_part_on_me;
 
 } GridBlock;
+
+char* xor_cypher(char* text, char* key)
+{
+    int text_len = strlen(text);
+    int key_len = strlen(key);
+    char* new = (char*)malloc(text_len + 1);
+    int k = key_len < text_len ? key_len : text_len;
+    int i, j = 0;
+    for (i = 0; i < text_len; i++, j = (j + 1) % k)
+        new[i] = text[i] ^ key[j];
+    new[i] = '\0';
+    return new; 
+}
+
+void* makeOrOpenHighscoreFile()
+{
+    FILE* file = fopen("snake.bin", "rb");
+    if (file == NULL)
+    {
+        file = fopen("snake.bin", "w+b");
+        if(file == NULL)
+            return NULL;
+        fwrite(xor_cypher(highscore, key), sizeof(char), 19, file);
+        int zero = 0;
+        fwrite(&zero, sizeof(int), 1, file);
+        fseek(file, 0, SEEK_SET);
+        return file;
+    }
+    else
+    {
+        int len = strlen(highscore);
+        char* label = (char*)malloc(len + 1);
+        fread(label, sizeof(char), 19, file);
+        label[19] = '\0';
+        if(strcmp(xor_cypher(label, key), highscore) == 0)
+        {
+            fseek(file, 0, SEEK_SET);
+            return file;
+        }
+        fclose(file);
+        FILE* file = fopen("snake.bin", "w+b");
+        if(file == NULL)
+            return NULL;
+        fwrite(xor_cypher(highscore, key), sizeof(char), 19, file);
+        int zero = 0;
+        fwrite(&zero, sizeof(int), 1, file);
+        fseek(file, 0, SEEK_SET);
+        return file;
+    }
+}
+
+int readHighscore(FILE* file)
+{
+    fseek(file, 19, SEEK_SET);
+    int hs;
+    fread(&hs, sizeof(int), 1, file);
+    fseek(file, 0, SEEK_SET);
+    return hs;
+}
+
+void updateHighscore(FILE* file, int new_hs)
+{
+    fseek(file, 19, SEEK_SET);
+    //printf("Upisujem: %d\n", new_hs);
+    int score;
+    fwrite(&new_hs, sizeof(int), 1, file);
+    uint32_t i, k;
+    unsigned char j;
+    srand(time(NULL));
+    i = rand() % ((uint32_t)~0);
+    j = rand() % ((unsigned char)~0);
+    k = rand() % ((uint32_t)~0);
+    fwrite(&i, sizeof(uint32_t), 1, file);
+    fwrite(&j, sizeof(char), 1, file);
+    fwrite(&k, sizeof(uint32_t), 1, file);
+    fseek(file, 0, SEEK_SET);
+}
 
 void drawBlock(SDL_Renderer* renderer, GridBlock* block)
 {
@@ -107,6 +187,16 @@ GridBlock*** allocGrid()
         for (int j = 0; j < COLS; j++)
             grid[i][j] = allocBlock(i , j);
     return grid;
+}
+
+void clearGrid(GridBlock*** grid)
+{
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+        {
+            grid[i][j]->fruit = false;
+            grid[i][j]->snake_part_on_me = NULL;
+        } 
 }
 
 void drawGrid(SDL_Renderer* renderer, GridBlock*** grid)
@@ -194,16 +284,28 @@ void updateSnakePosition(bool* out_arr, SnakePart* head, GridBlock*** grid, Uint
         occupied_grid_blocks[next_new_i * COLS + next_new_j] = true;
     }
     out_arr[0] = killed;
-    //if(grid[i][j]->fruit)
-       // eatFruit(headptr);
+}
+
+void addPoints(Uint64 time_of_last_eaten, Uint64 time_of_before_last_eaten,  long long* score)
+{
+    if (time_of_last_eaten - time_of_before_last_eaten < 3000)
+    {
+        *score += time_bonus;
+        time_bonus += 10;
+    }
+    else
+    {
+        *score += 5;
+        time_bonus = BASE_TIME_BONUS;
+    }
 }
 
 void destroySnake(SDL_Renderer* renderer, SnakePart* head, GridBlock*** grid, int fps)
 {
     SnakePart* current = head;
-    Uint32 wanted_frame_time = 1000 / fps;
-    Uint32 beg_of_frame;
-    Uint32 frame_time;
+    Uint64 wanted_frame_time = (Uint64) 1000 / fps;
+    Uint64 beg_of_frame;
+    Uint64 frame_time;
     while (current != NULL)
     {
         beg_of_frame = SDL_GetTicks();
@@ -229,9 +331,8 @@ void placeFruit(GridBlock*** grid)
     long long rand1 = rand();
     srand(time(NULL));
     long long rand2 = rand();
-    // printa za rand2 da je nula a nije najjaca forica, al radi ok...
+    // printa za rand2 da je nula a nije najjaca forica, al radi ok..., nije losa distribucija vockica
     int random = (rand1 * rand2) % len;
-    printf("random = %d \n", random);
     int row = random / COLS;
     int col = random % COLS;
     if (!occupied_grid_blocks[random])
@@ -256,83 +357,117 @@ void placeFruit(GridBlock*** grid)
 
 int main(int argc, char** argv)
 {
-    srand(time(NULL));
-    int r1 = rand();
-    int r2 = rand();
-    printf("%d, %d", r1, r2);
+    FILE* file = (FILE*)makeOrOpenHighscoreFile();
+    int hs;
     if(!SDL_Init(SDL_INIT_VIDEO))
-        printf("Neuspesna inicijalizacija SDL video moda.");
+    {
+        printf("Neuspesna inicijalizacija SDL video moda.\n");
+        return -1;
+    }
     int len = ROWS * COLS;
     for (int i = 0; i < len; i++)
         occupied_grid_blocks[i] = false; 
 
-    SDL_Window* window = SDL_CreateWindow("Snake game", BLOCK_SIZE * COLS, BLOCK_SIZE * ROWS + 50, SDL_WINDOW_RESIZABLE);
+    SDL_Window* window = SDL_CreateWindow("Snake game", BLOCK_SIZE * COLS, BLOCK_SIZE * ROWS + 100, SDL_WINDOW_RESIZABLE);
     SDL_SetWindowResizable(window, false);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
     SDL_Event event;
     GridBlock*** grid = allocGrid();
-    SnakePart* snake_head = makeSnake(grid);
-    SnakePart* snake_tail = snake_head->next_part->next_part;
-    Uint32 last_arrow_input = SDLK_DOWN;
-    // int x, y;
-    // SDL_GetWindowPosition(window, &x, &y);
-    // printf("%d, %d", x, y);
-    // int top, left, bottom, right, abs_x, abs_y;
-    //if (SDL_GetWindowBordersSize(window, &top, &left, &bottom, &right) == 0)
+
     Uint32 fps = 10;
-    Uint32 wanted_frame_time = 1000 / fps;
-    Uint32 beg_of_frame;
-    Uint32 frame_time;
+    Uint64 wanted_frame_time = (Uint64) 1000 / fps;
+    Uint64 beg_of_frame;
+    Uint64 frame_time;
     bool running = true;
-    bool killed = false;
-    bool grow = false;
-    placeFruit(grid);
-    while(running && !killed)
-    {
-        beg_of_frame = SDL_GetTicks();
-        while(SDL_PollEvent(&event))
+    while (running)
+    {   
+        SnakePart* snake_head = makeSnake(grid);
+        SnakePart* snake_tail = snake_head->next_part->next_part;
+        Uint32 last_arrow_input = SDLK_DOWN;
+        clearGrid(grid);
+        bool killed = false;
+        bool grow = false;
+        long long points = 0;
+        placeFruit(grid);
+        Uint64 time_last_eaten = SDL_GetTicks();
+        if (file != NULL)
         {
-            if ((event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE) || event.type == SDL_EVENT_QUIT)
-                running = false;
-            switch (event.key.key) 
-            {
-                case SDLK_UP:
-                    if(last_arrow_input != SDLK_DOWN)
-                        last_arrow_input = SDLK_UP;
-                    break;
-                case SDLK_DOWN:
-                    if(last_arrow_input != SDLK_UP)
-                        last_arrow_input = SDLK_DOWN;
-                    break;
-                case SDLK_LEFT:
-                    if(last_arrow_input != SDLK_RIGHT)
-                        last_arrow_input = SDLK_LEFT;
-                    break;
-                case SDLK_RIGHT:
-                    if(last_arrow_input != SDLK_LEFT)
-                        last_arrow_input = SDLK_RIGHT; 
-            }
+            hs = readHighscore(file);
+            printf("Rekord -> %d\n", hs);
         }
-        bool outarr[2];
-        updateSnakePosition(outarr, snake_head, grid, last_arrow_input, grow);
-        killed = outarr[0];
+        while(running && !killed)
+        {
+            beg_of_frame = SDL_GetTicks();
+            if (SDL_PollEvent(&event))
+            {
+                if ((event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE) || event.type == SDL_EVENT_QUIT)
+                    running = false;
+                switch (event.key.key) 
+                {
+                    case SDLK_UP:
+                        if(last_arrow_input != SDLK_DOWN)
+                            last_arrow_input = SDLK_UP;
+                        break;
+                    case SDLK_DOWN:
+                        if(last_arrow_input != SDLK_UP)
+                            last_arrow_input = SDLK_DOWN;
+                        break;
+                    case SDLK_LEFT:
+                        if(last_arrow_input != SDLK_RIGHT)
+                            last_arrow_input = SDLK_LEFT;
+                        break;
+                    case SDLK_RIGHT:
+                        if(last_arrow_input != SDLK_LEFT)
+                            last_arrow_input = SDLK_RIGHT; 
+                }
+            }
+            bool outarr[2];
+            updateSnakePosition(outarr, snake_head, grid, last_arrow_input, grow);
+            killed = outarr[0];
+            if (killed)       
+                break;
+            grow = outarr[1];
+            if (grow)
+            {
+                placeFruit(grid);
+                Uint64 time_now_eaten = SDL_GetTicks();
+                addPoints(time_now_eaten, time_last_eaten, &points);
+                printf("Poeni -> %d\n", points);
+                time_last_eaten = time_now_eaten;
+            }
+            drawGrid(renderer, grid);
+
+            SDL_RenderPresent(renderer);
+            SDL_RenderClear(renderer);
+
+            frame_time = SDL_GetTicks() - beg_of_frame; 
+            if(frame_time < wanted_frame_time)
+                SDL_Delay(wanted_frame_time - frame_time);
+        }
         if (killed)
-            break;
-        grow = outarr[1];
-        if (grow)
-            placeFruit(grid);
-        
-        drawGrid(renderer, grid);
-
-        SDL_RenderPresent(renderer);
-        SDL_RenderClear(renderer);
-
-        frame_time = SDL_GetTicks() - beg_of_frame; 
-        if(frame_time < wanted_frame_time)
-            SDL_Delay(wanted_frame_time - frame_time);
+        {
+            if (points > hs)
+                {
+                    updateHighscore(file, points);
+                    printf("Novi rekord! -> %d\n", readHighscore(file));
+                    hs = points;
+                }
+            destroySnake(renderer, snake_head, grid, 30);
+        }
+        bool restart = false;
+        while(!restart && running)
+        {
+            while(SDL_PollEvent(&event))
+            {
+                if ((event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_ESCAPE) 
+                    || event.type == SDL_EVENT_QUIT)
+                    running = false;
+                else if (event.key.key == SDLK_R)
+                    restart = true;
+            }   
+        }
     }
-    if (killed)
-        destroySnake(renderer, snake_head, grid, fps);
+    fclose(file);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
